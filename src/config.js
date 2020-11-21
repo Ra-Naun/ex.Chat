89f3e63a6524b886075f7edc26f1b4ec49850ea5
Dmitry <dmitriy_slavskiy@mail.ru>
2020-11-21 14:13:23 +0400

    0

---
+++ src/config.js
@@ -0,0 +1,11 @@
+//обычно этот файл в .gitignore, но в этом проекте не включил его в список, чтоб проект работал в github pages
+var API_THUMBSNAP_UPLOAD_IMAGES = {
+    URL:'https://thumbsnap.com/api/upload',
+    KEY:'0000116cc8abba6e32a4e5cbe49a39bf',
+} 
+var SOCKET_URL = 'https://nest-zuz.herokuapp.com/';
+
+export {
+    API_THUMBSNAP_UPLOAD_IMAGES,
+    SOCKET_URL,
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from '../images/logo.svg';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="Home">
+      <header className="Home-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="Home-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -22,4 +22,5 @@ function App() {
   );
 }
 
-export default App;
+
+export default Home;
